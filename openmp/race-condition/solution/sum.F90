c5ebad9196d695208556b2d0e8160437fdfb5a1e
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-09-25 16:42:05 +0300

    Remove reduction and critical from model solution

--- openmp/race-condition/solution/sum.F90
+++ openmp/race-condition/solution/sum.F90
@@ -25,39 +25,4 @@ program vectorsum
   !$omp end parallel do
   write(*,*) 'Sum with data race:                              ', sum
 
-  sum = 0
-  ! Dot product using critical section = SERIAL CODE
-  !$omp parallel do default(shared) private(i)
-  do i = 1, nx
-     !$omp critical
-     sum = sum + vecA(i)
-     !$omp end critical
-  end do
-  !$omp end parallel do
-  write(*,*) 'Sum using critical section:                      ', sum
-
-  sum = 0
-  ! Dot product using reduction
-  !$omp parallel do default(shared) private(i) reduction(+:sum)
-  do i = 1, nx
-     sum = sum + vecA(i)
-  end do
-  !$omp end parallel do
-  write(*,*) 'Sum using reduction:                             ', sum
-
-  sum = 0
-  ! Dot product using private variable and critical secion
-  !$omp parallel default(shared) private(i, psum)
-  psum = 0
-  !$omp do
-  do i = 1, nx
-     psum = psum + vecA(i)
-  end do
-  !$omp end do
-  !$omp critical
-  sum = sum + psum
-  !$omp end critical
-  !$omp end parallel
-  write(*,*) 'Sum using private variable and critical section: ', sum
-
 end program vectorsum
--- openmp/race-condition/solution/sum.c
+++ openmp/race-condition/solution/sum.c
@@ -26,39 +26,5 @@ int main(void)
     printf("Sum with data race:                              %ld\n",
            sum);
 
-    sum = 0.0;
-    /* Dot product using critical section = SERIAL CODE! */
-    #pragma omp parallel for default(shared) private(i)
-    for (i = 0; i < NX; i++) {
-        #pragma omp critical(dummy)
-        sum += vecA[i];
-    }
-    printf("Sum using critical section:                      %ld\n",
-           sum);
-
-    sum = 0.0;
-    /* Dot product using private partial sums and critical section */
-    #pragma omp parallel default(shared) private(i, psum)
-    {
-        psum = 0.0;
-        #pragma omp for
-        for (i = 0; i < NX; i++) {
-            psum += vecA[i];
-        }
-        #pragma omp critical(par_sum)
-        sum += psum;
-    }
-    printf("Sum using private variable and critical section: %ld\n",
-           sum);
-
-    sum = 0.0;
-    /* Dot product using reduction */
-    #pragma omp parallel for default(shared) private(i) reduction(+:sum)
-    for (i = 0; i < NX; i++) {
-        sum += vecA[i];
-    }
-    printf("Reduction sum:                                   %ld\n",
-           sum);
-
     return 0;
 }
