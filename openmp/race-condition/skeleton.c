be65dea58901db99034d61be5da299a255814ffc
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-09-25 16:35:46 +0300

    Race condition in parallel sum

---
+++ openmp/race-condition/skeleton.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define NX 102400
+
+int main(void)
+{
+    long vecA[NX];
+    long sum, psum, sumex;
+    int i;
+
+    /* Initialization of the vectors */
+    for (i = 0; i < NX; i++) {
+        vecA[i] = (long) i + 1;
+    }
+
+    sumex = (long) NX * (NX + 1) / ((long) 2);
+    printf("Arithmetic sum formula (exact):                  %ld\n",
+           sumex);
+
+    sum = 0.0;
+    /* TODO: Parallelize computation */
+    for (i = 0; i < NX; i++) {
+        sum += vecA[i];
+    }
+    printf("Sum: %ld\n", sum);
+
+    return 0;
+}
