0bcab255464f1ed93520c8458bcb4c1e5d93d592
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-10-05 10:12:21 +0300

    Communicator demo

---
+++ demos/mpi-split.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define NTASKS 4
+
+void print_buffers(int *printbuffer, int *sendbuffer, int buffersize);
+void init_buffers(int *sendbuffer, int *recvbuffer, int buffersize);
+
+
+int main(int argc, char *argv[])
+{
+    int ntasks, rank;
+    int sendbuf[2 * NTASKS], recvbuf[2 * NTASKS];
+    int printbuf[2 * NTASKS * NTASKS];
+    int color, subrank, key;
+    MPI_Comm subcomm;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (ntasks != NTASKS) {
+        if (rank == 0) {
+            fprintf(stderr, "Run this program with %i tasks.\n", NTASKS);
+        }
+        MPI_Abort(MPI_COMM_WORLD, -1);
+    }
+
+    /* Initialize message buffers */
+    init_buffers(sendbuf, recvbuf, 2 * NTASKS);
+
+    /* Print data that will be sent */
+    print_buffers(printbuf, sendbuf, 2 * NTASKS);
+
+    if (rank % 2 == 0) {
+        color = 0;
+    } else {
+        color = 1;
+    }
+    key = rank;
+    MPI_Comm_split(MPI_COMM_WORLD, color, key, &subcomm);
+    MPI_Comm_rank(subcomm, &subrank);
+
+    /* Broadcast in a sub-communicator */
+    MPI_Bcast(sendbuf, 2 * NTASKS, MPI_INT, 0, subcomm);
+
+    /* Print data that was received */
+    print_buffers(printbuf, sendbuf, 2 * NTASKS);
+
+    MPI_Finalize();
+    return 0;
+}
+
+
+void init_buffers(int *sendbuffer, int *recvbuffer, int buffersize)
+{
+    int rank, i;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    for (i = 0; i < buffersize; i++) {
+        recvbuffer[i] = -1;
+        sendbuffer[i] = i + buffersize * rank;
+    }
+}
+
+
+void print_buffers(int *printbuffer, int *sendbuffer, int buffersize)
+{
+    int i, j, rank, ntasks;
+
+    MPI_Gather(sendbuffer, buffersize, MPI_INT,
+               printbuffer, buffersize, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+
+    if (rank == 0) {
+        for (j = 0; j < ntasks; j++) {
+            printf("Task %i:", j);
+            for (i = 0; i < buffersize; i++) {
+                printf(" %2i", printbuffer[i + buffersize * j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
