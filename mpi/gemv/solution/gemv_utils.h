731520a4bf544015251a1732783f95daed4926aa
juhani.kataja <juhani.kataja@csc.fi>
2019-10-21 16:50:11 +0300

    added gemv_utils.{F90,h} files

---
+++ mpi/gemv/solution/gemv_utils.h
@@ -0,0 +1,27 @@
+void allocate_dense(size_t rows,size_t  cols, double*** dense) {
+
+  *dense = malloc(sizeof(double)*rows);
+  **dense = malloc(sizeof(double)*rows*cols);
+
+  for (size_t i=0; i < rows; i++ ) {
+    (*dense)[i] = (*dense)[0] + i*cols;
+  }
+
+}
+
+void print_mat(double** A, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      printf("%f ", A[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void print_vec(double* b, size_t rows) {
+  for (size_t i = 0; i < rows; i++) {
+    printf("%f\n", b[i]);
+  }
+}
+
+double sum_vec(double* vec, size_t rows);
