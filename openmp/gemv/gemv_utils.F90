717e6ff934e30b89e94b5c9b80c520dac68dda24
Sami Ilvonen <sami.ilvonen@csc.fi>
2019-10-21 17:08:59 +0300

    More indenting updates

--- openmp/gemv/gemv_utils.F90
+++ openmp/gemv/gemv_utils.F90
@@ -3,15 +3,15 @@ subroutine print_vec(x)
   real(kind=dp) :: x(:)
   integer(kind=ik) :: i
   do i = 1,size(x,1)
-    print *, x(i)
+     print *, x(i)
   end do
-end subroutine
+end subroutine print_vec
 
 subroutine print_mat(A)
   implicit none
   real(kind=dp) :: A(:,:)
   integer(kind=ik) :: i
   do i = 1,size(A,1)
-    print *, A(i,:)
+     print *, A(i,:)
   end do
-end subroutine
+end subroutine print_mat
--- openmp/gemv/solution/gemv.F90
+++ openmp/gemv/solution/gemv.F90
@@ -1,19 +1,18 @@
-
 program gemv_test
-use iso_fortran_env, only: int64, real64
-implicit none
-integer, parameter :: ik = int64
-integer, parameter :: dp = real64
+  use iso_fortran_env, only: int64, real64
+  implicit none
+  integer, parameter :: ik = int64
+  integer, parameter :: dp = real64
 
-integer(kind=ik), parameter  :: rows = 10000,cols = 10000
-real(kind=dp), allocatable :: A(:,:), b(:), x(:)
-integer(kind=ik) :: i
+  integer(kind=ik), parameter  :: rows = 10000,cols = 10000
+  real(kind=dp), allocatable :: A(:,:), b(:), x(:)
+  integer(kind=ik) :: i
 
-allocate(A(rows,cols), b(rows), x(cols))
-call make_hilbert_mat(A)
-x = [(1_dp*i, i=1,cols)]
-b = 0.0_dp
-call gemv(A, x, b)
+  allocate(A(rows,cols), b(rows), x(cols))
+  call make_hilbert_mat(A)
+  x = [(1_dp*i, i=1,cols)]
+  b = 0.0_dp
+  call gemv(A, x, b)
 
 #if 0
   call print_vec(x);
@@ -21,37 +20,38 @@ call gemv(A, x, b)
   call print_vec(b);
 #endif
 
-print *, 'sum(x) = ', sum(x), 'sum(Ax) = ', sum(b)
+  print *, 'sum(x) = ', sum(x), 'sum(Ax) = ', sum(b)
+
 contains
 
-subroutine make_hilbert_mat(A)
-  real(kind=dp), intent(out) :: A(:,:)
-  integer(kind=ik) :: i, j
-!$omp parallel private(i,j)
-!$omp do
-  do j = 1, size(A,2)
-    do i = 1, size(A,1)
-      A(i,j) = 1_dp/(1.0_dp*(i+j-1))
+  subroutine make_hilbert_mat(A)
+    real(kind=dp), intent(out) :: A(:,:)
+    integer(kind=ik) :: i, j
+    !$omp parallel private(i,j)
+    !$omp do
+    do j = 1, size(A,2)
+       do i = 1, size(A,1)
+          A(i,j) = 1_dp/(1.0_dp*(i+j-1))
+       end do
     end do
-  end do
-!$omp end do
-!$omp end parallel
-end subroutine
+    !$omp end do
+    !$omp end parallel
+  end subroutine make_hilbert_mat
 
-subroutine gemv(A, x, b)
-  real(kind=dp), intent(in) :: A(:,:), x(:)
-  real(kind=dp), intent(out) :: b(:)
-  real(kind=dp) :: rowsum 
-  integer(kind=ik) :: i, j
-  !$omp parallel do private(i,j) reduction(+:b)
-  do j = 1, size(A, 2)
-    do i = 1, size(A, 1)
-      b(i) = b(i) + A(i,j)*x(j)
+  subroutine gemv(A, x, b)
+    real(kind=dp), intent(in) :: A(:,:), x(:)
+    real(kind=dp), intent(out) :: b(:)
+    real(kind=dp) :: rowsum 
+    integer(kind=ik) :: i, j
+    !$omp parallel do private(i,j) reduction(+:b)
+    do j = 1, size(A, 2)
+       do i = 1, size(A, 1)
+          b(i) = b(i) + A(i,j)*x(j)
+       end do
     end do
-  end do
-  !$omp end parallel do
-end subroutine
+    !$omp end parallel do
+  end subroutine gemv
 
 #include "gemv_utils.F90"
 
-end program
+end program gemv_test
